6bc69367bf13692e1f445a1407e543c901ab922e
4shhal <ashhalchohan2005@gmail.com>
2026-07-18 17:23:29 +0500

    Successfully remove venv tracking and add project files

---
+++ Obesity Risk/main.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+file_path = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/GkDzb7bWrtvGXdPOfk6CIg/Obesity-level-prediction-dataset.csv"
+data = pd.read_csv(file_path)
+print(data.head())
